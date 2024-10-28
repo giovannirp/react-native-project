8b052c91fbdc333701b9654aad2a247c713bd0ab
Giovanni <giovanni_ge@hotmail.com>
2024-10-28 17:03:37 -0300

    feat

--- projeto-general/src/Pages/Sobre.js
+++ projeto-general/src/Pages/Sobre.js
@@ -1,6 +1,6 @@
-import React, { useEffect, useState } from 'react'
-import { StyleSheet, Text, View } from 'react-native'
-import axios from 'axios';
+import React, { useEffect, useState } from "react";
+import { Alert, Button, ScrollView, StyleSheet, Text, View } from "react-native";
+import axios from "axios";
 
 export default function Sobre() {
   const [contatos, setContatos] = useState([]);
@@ -10,33 +10,55 @@ export default function Sobre() {
     axios
       .get("http://10.0.2.2:3000/contatos")
       .then((resposta) => {
-        setContatos(resposta.data)
+        setContatos(resposta.data);
       })
       .catch((error) => {
-        console.error("Erro ao buscar contatos", error)
+        console.error("Erro ao buscar contatos", error);
+      });
+  };
+
+  // Função para excluir um contato
+  const deleteContato = (id) => {
+    axios
+      .delete(`http://10.0.2.2:3000/contatos/${id}`)
+      .then(() => {
+        // Atualizar a lista de contato
+        setContatos(contatos.filter((contato) => contato.id !== id));
+        Alert.alert("Sucesso, contato Excluido com sucesso");
       })
-  }
+      .catch((error) => {
+        console.error("Erro ao excluir contato", error);
+        Alert.alert("Erro, não foi possivel excluir");
+      });
+  } 
 
   // Use o useEffect para buscar dados
   useEffect(() => {
     listContatos();
-  }, [])
+  }, []);
 
   return (
-    <View style={styles.container}>
+    <ScrollView>
+      <View style={styles.container}>
         <Text style={styles.title}>Lista de contatos</Text>
         {contatos.length > 0 ? (
           contatos.map((contato, index) => (
             <View key={index} style={styles.contatoItem}>
               <Text>{contato.nome}</Text>
               <Text>{contato.telefone}</Text>
+              <Button 
+                title="Excluir" 
+                color="orange"
+                onPress={() => deleteContato(contato.id)} 
+              />
             </View>
           ))
         ) : (
           <Text style={styles.noContacts}>Nenhum contato disponível</Text>
         )}
-    </View>
-  )
+      </View>
+    </ScrollView>
+  );
 }
 
 // Estilos para a tela
@@ -47,17 +69,17 @@ const styles = StyleSheet.create({
   title: {
     fontSize: 24,
     fontWeight: "bold",
-    marginBottom: 10
+    marginBottom: 10,
   },
   contatoItem: {
     padding: 10,
     borderBottomWidth: 1,
-    borderBottomColor: "#ccc"
-  }, 
+    borderBottomColor: "#ccc",
+  },
   noContacts: {
     fontSize: 16,
     color: "gray",
     textAlign: "center",
-    marginTop: 20
-  }
+    marginTop: 20,
+  },
 });
